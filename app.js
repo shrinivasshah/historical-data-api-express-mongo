4bbd1a71852f08e1c732b5f04af5b7e127b46c15
shrinivasshah <shrinivasshah05@gmail.com>
2021-01-01 20:52:42 +0530

    bug fix

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ con.on("open", () => {
 
 app.use("/coins", mainRouter);
 
-app.listen(process.env.port || 9000, () => {
+app.listen(process.env.PORT || 9000, () => {
   console.log("Server started successfully on\n  http://localhost:9000");
 });
 
